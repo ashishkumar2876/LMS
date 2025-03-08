0448ece2750bbe33ef81f19b48e841ea92b7f951
ashishkumar2876 <157505007+ashishkumar2876@users.noreply.github.com>
2025-03-08 23:39:27 +0530

    all changes added

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT=process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:`${process.env.FRONTEND_URL}`,
     credentials:true
 }));
 app.use(cookieParser());
